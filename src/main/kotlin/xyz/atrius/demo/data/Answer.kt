e0b289c3ef12c8348a04c66d24eda2629f6b2130
Atrius <atriusedm@gmail.com>
2022-01-22 06:28:35 -0500

    Add Answer documentation

--- src/main/kotlin/xyz/atrius/demo/data/Answer.kt
+++ src/main/kotlin/xyz/atrius/demo/data/Answer.kt
@@ -1,5 +1,13 @@
 package xyz.atrius.demo.data
 
+/**
+ * @author Atrius
+ *
+ * Models a simple answer response to the requesting client.
+ *
+ * @property equation The parsed equation taken from the given input.
+ * @property result   The resulting value of the calculation.
+ */
 data class Answer(
     val equation: String,
     val result: Double
