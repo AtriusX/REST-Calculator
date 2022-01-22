7f82dedbf9820bc148e16e40a35e42380995ae57
Atrius <atriusedm@gmail.com>
2022-01-22 17:20:35 -0500

    Simplify validator a bit

--- src/main/kotlin/xyz/atrius/demo/bridge/validation/ExpressionValidator.kt
+++ src/main/kotlin/xyz/atrius/demo/bridge/validation/ExpressionValidator.kt
@@ -5,13 +5,24 @@ import xyz.atrius.demo.data.AppError
 /**
  * @author Atrius
  *
- *
+ * This validator is used for validating the input as a valid mathematical
+ * expression. This can be used ahead of a [Parser][xyz.atrius.demo.bridge.parsing.Parser]
+ * object to determine if the given input is able to be properly used. If the
+ * validator detects a formatting issue, then any issues found are raised via
+ * an [AppError] object.
  */
 class ExpressionValidator : Validator {
 
     private val ops: Set<Char> =
         setOf('+', '-', '*', '/', '^')
 
+    /**
+     * Validate the input to check if the format matches that of a valid
+     * math expression.
+     *
+     * @param input The input string to validate.
+     * @return      Any raised [AppError] instances, or null if successful.
+     */
     override fun validate(input: String): AppError? {
         var depth = 0
         var prev: Char? = null
@@ -20,21 +31,26 @@ class ExpressionValidator : Validator {
         for (c in input) {
             when (c) {
                 // Clears the current item and skips to next character
-                ' '              -> {
+                ' ' -> {
                     val error = cur.checkNumber()
                     // Return any raised errors or continue to next character
-                    if (error != null)
-                        return error else continue
+                    if (error != null) {
+                        return error
+                    } else {
+                        // Flush and continue
+                        cur.clear()
+                        continue
+                    }
                 }
                 // Open a new scope
-                '('              -> {
+                '(' -> {
                     // Check if the scope starts right after a number
                     if (cur.isNotEmpty())
                         return AppError.UnexpectedSubexpression
                     depth++
                 }
                 // Close the current scope
-                ')'              -> {
+                ')' -> {
                     // Terminate at the end of a scope (must follow the end
                     // of another scope or constant), or if we cause an
                     // underflow the scope depth
@@ -43,7 +59,7 @@ class ExpressionValidator : Validator {
                     depth--
                 }
                 // Track operators
-                in ops           -> {
+                in ops -> {
                     // Prevent operators aside from '-' from being places
                     // at the beginning of an expression
                     if (prev == null && c != '-')
@@ -61,7 +77,7 @@ class ExpressionValidator : Validator {
                 // Build numbers from numeric values
                 in '0'..'9', '.' -> cur.append(c)
                 // Any other character is considered invalid
-                else             -> return AppError.InvalidSymbol
+                else -> return AppError.InvalidSymbol
             }
             prev = c
         }
@@ -78,10 +94,7 @@ class ExpressionValidator : Validator {
     private fun StringBuilder.checkNumber(): AppError? {
         // We are tracking a multi-character item (a constant value)
         // If the current value is not a valid number then an error is raised
-        if (isNotEmpty() && !isNumber())
-            return AppError.InvalidConstant
-        // Flush and continue
-        clear()
-        return null
+        return if (isNotEmpty() && !isNumber())
+            AppError.InvalidConstant else null
     }
 }
