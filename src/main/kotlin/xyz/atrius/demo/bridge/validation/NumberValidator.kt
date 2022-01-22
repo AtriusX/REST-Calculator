75fa62dc91c72980244a20b4f9678f6e17dbcd7c
Atrius <atriusedm@gmail.com>
2022-01-22 17:41:58 -0500

    Add NumberValidator documentation

--- src/main/kotlin/xyz/atrius/demo/bridge/validation/NumberValidator.kt
+++ src/main/kotlin/xyz/atrius/demo/bridge/validation/NumberValidator.kt
@@ -2,8 +2,22 @@ package xyz.atrius.demo.bridge.validation
 
 import xyz.atrius.demo.data.AppError
 
+/**
+ * @author Atrius
+ *
+ * This validator is used to determine if a given input matches a valid
+ * numeric format. If valid, the result is null, otherwise an [AppError.InvalidConstant]
+ * error is raised to the caller.
+ */
 class NumberValidator : Validator {
 
+    /**
+     * Validates that the input is a valid positive or negative
+     * floating-point number.
+     *
+     * @param input The input string to validate.
+     * @return      Null if successful, or [AppError.InvalidConstant] if an issue is found.
+     */
     override fun validate(input: String): AppError? {
         println("Input: '$input': ${input.isNumber()}")
         return if (input.isNumber())
