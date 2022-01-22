fb69cef46b1b1f869d278129f866ba44d78016e9
Atrius <atriusedm@gmail.com>
2022-01-22 14:18:24 -0500

    Add Validator definition

---
+++ src/main/kotlin/xyz/atrius/demo/bridge/validation/Validator.kt
@@ -0,0 +1,21 @@
+package xyz.atrius.demo.bridge.validation
+
+/**
+ * @author Atrius
+ *
+ * Validators are used to test an input for validity on a given criteria function.
+ * This is primarily useful for ensuring that our provided data does not raise any
+ * exceptions or throw any errors when parsed by the application at a later point.
+ */
+interface Validator {
+
+    /**
+     * Validates the input [String] for validity based on a given criteria function.
+     * This allows use to determine if certain errors might be raised at a later point.
+     * If this function returns null, then it is considered a successful validation.
+     *
+     * @param input The input to be validated.
+     * @return      An [Error] object if an error is raised, or null if no error is found.
+     */
+    fun validate(input: String): Error?
+}
