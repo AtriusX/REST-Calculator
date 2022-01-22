b0a1c2554fcc827c5d4827a7f9c802b176a9dd46
Atrius <atriusedm@gmail.com>
2022-01-22 14:04:05 -0500

    Add a basic expression parser definition

---
+++ src/main/kotlin/xyz/atrius/demo/parser/ExpressionParser.kt
@@ -0,0 +1,25 @@
+package xyz.atrius.demo.parser
+
+import arrow.core.Either
+import xyz.atrius.demo.math.Node
+
+/**
+ * @author Atrius
+ *
+ * Expression parsers are used for scanning over an input string and converting it into the
+ * proper [Node] representation. This allows us to describe mathematical expressions from
+ * natural language instead of through programmatic constructs. This system will then be
+ * used as the bridge from natural language to logical constructs.
+ */
+interface ExpressionParser {
+
+    /**
+     * Parses an input [String] into a [Node] tree structure. Alternatively, if parsing
+     * fails for any reason, an [Error] is instead returned. If parsing succeeds, the
+     * resulting node can be evaluated for its result.
+     *
+     * @param input The string to attempt parsing for.
+     * @return      The determined node tree, or an error if parsing fails.
+     */
+    fun parse(input: String): Either<Error, Node>
+}
