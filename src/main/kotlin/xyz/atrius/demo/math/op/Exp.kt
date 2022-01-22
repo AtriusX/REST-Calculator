06e169e7214410f5b7bb1d252a1dfc2aff63bee7
Atrius <atriusedm@gmail.com>
2022-01-22 05:52:20 -0500

    Add exponent operator

---
+++ src/main/kotlin/xyz/atrius/demo/math/op/Exp.kt
@@ -0,0 +1,24 @@
+package xyz.atrius.demo.math.op
+
+import xyz.atrius.demo.math.Node
+import kotlin.math.pow
+
+/**
+ * @author Atrius
+ *
+ * Performs an exponent operation on a given set of child expressions.
+ *
+ * @property left  The left-side subexpression for this operation.
+ * @property right The right-side subexpression for this operation.
+ */
+class Exp(
+    left: Node,
+    right: Node
+) : BinaryOperator(left, right) {
+
+    override val operator: Char = '^'
+
+    override fun operation(left: Node, right: Node): Double {
+        return left.evaluate().pow(right.evaluate())
+    }
+}
