6be8b1dfafe0435892c687878d31ae93412a9d36
Atrius <atriusedm@gmail.com>
2022-01-23 15:47:43 -0500

    Simplify ValidatorTests

--- src/test/kotlin/xyz/atrius/demo/bridge/validation/ValidatorTests.kt
+++ src/test/kotlin/xyz/atrius/demo/bridge/validation/ValidatorTests.kt
@@ -13,50 +13,46 @@ import org.springframework.boot.test.context.SpringBootTest
 class ValidatorTests {
 
     @Test
-    fun `Test number validation`() {
-        with(NumberValidator()) {
-            // Valid values
-            assert(validate("0") == null)
-            assert(validate("10") == null)
-            assert(validate("-100") == null)
-            assert(validate("4.5") == null)
-            assert(validate(".03") == null)
-            // Invalid values
-            assert(validate("") != null)
-            assert(validate("undefined") != null)
-            assert(validate("null") != null)
-            assert(validate("abcdef") != null)
-            assert(validate("!!") != null)
-        }
+    fun `Test number validation`() = with(NumberValidator()) {
+        // Valid values
+        assert(validate("0") == null)
+        assert(validate("10") == null)
+        assert(validate("-100") == null)
+        assert(validate("4.5") == null)
+        assert(validate(".03") == null)
+        // Invalid values
+        assert(validate("") != null)
+        assert(validate("undefined") != null)
+        assert(validate("null") != null)
+        assert(validate("abcdef") != null)
+        assert(validate("!!") != null)
     }
 
     @Test
-    fun `Test expression validation`() {
-        with(ExpressionValidator()) {
-            // Valid expressions
-            assert(validate("0") == null)
-            assert(validate("2 + 2") == null)
-            assert(validate("(2 / 3) + 6") == null)
-            assert(validate("(2        / 3)              +          6    ") == null)
-            assert(validate("(2 / (3 + 10)) + 6 ^ 3") == null)
-            assert(validate("2 ^ 3 ^ 4 / 5 * (5 - 4 + -3)") == null)
-            assert(validate("-3") == null)
-            assert(validate("0 * 0 - 0 / 0 + 0") == null)
-            // Invalid expressions
-            assert(validate("()") != null)
-            assert(validate("+") != null)
-            assert(validate("2 ++ 10") != null)
-            assert(validate("2(100)") != null)
-            assert(validate("(((") != null)
-            assert(validate("(2 / 3) + 6 + (") != null)
-            assert(validate("+3423") != null)
-            assert(validate("3.0.0") != null)
-            assert(validate("3.0.0 + 56") != null)
-            assert(validate("invalid") != null)
-            assert(validate("++-*/") != null)
-            assert(validate("--0") != null)
-            assert(validate("0--") != null)
-            assert(validate(")))(((") != null)
-        }
+    fun `Test expression validation`() = with(ExpressionValidator()) {
+        // Valid expressions
+        assert(validate("0") == null)
+        assert(validate("2 + 2") == null)
+        assert(validate("(2 / 3) + 6") == null)
+        assert(validate("(2        / 3)              +          6    ") == null)
+        assert(validate("(2 / (3 + 10)) + 6 ^ 3") == null)
+        assert(validate("2 ^ 3 ^ 4 / 5 * (5 - 4 + -3)") == null)
+        assert(validate("-3") == null)
+        assert(validate("0 * 0 - 0 / 0 + 0") == null)
+        // Invalid expressions
+        assert(validate("()") != null)
+        assert(validate("+") != null)
+        assert(validate("2 ++ 10") != null)
+        assert(validate("2(100)") != null)
+        assert(validate("(((") != null)
+        assert(validate("(2 / 3) + 6 + (") != null)
+        assert(validate("+3423") != null)
+        assert(validate("3.0.0") != null)
+        assert(validate("3.0.0 + 56") != null)
+        assert(validate("invalid") != null)
+        assert(validate("++-*/") != null)
+        assert(validate("--0") != null)
+        assert(validate("0--") != null)
+        assert(validate(")))(((") != null)
     }
 }
