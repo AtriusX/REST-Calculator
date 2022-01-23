a3c98e7df29914fadbe60e866227f82fd54f228b
Atrius <atriusedm@gmail.com>
2022-01-22 19:28:08 -0500

    Get entire request URI instead of a path variable to fix division operator issues

--- src/main/kotlin/xyz/atrius/demo/controller/ApiController.kt
+++ src/main/kotlin/xyz/atrius/demo/controller/ApiController.kt
@@ -2,19 +2,22 @@ package xyz.atrius.demo.controller
 
 import arrow.core.Either
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import xyz.atrius.demo.data.*
+import javax.servlet.http.HttpServletRequest
 import kotlin.random.Random
+
+private const val API_BASE = "/calc"
 
 @RestController
 class ApiController {
 
-    @GetMapping("/calc/{equation}")
+    @GetMapping("$API_BASE/**")
     fun calc(
-        @PathVariable equation: String
+        request: HttpServletRequest,
     ): Either<AppError, Answer> {
-        return Either.Left(AppError.Custom("${Random.nextInt(10000)} Invalid equation: $equation"))
-//		return Either.Right(Answer("", 0.0))
+        return Either.Left(AppError.Custom(
+            "${Random.nextInt(10000)} Invalid equation: ${request.requestURI.substringAfter("$API_BASE/")}"
+        ))
     }
 }
